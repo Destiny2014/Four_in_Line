3417058bf7a664536addb481950247e46f80b570
Destiny2014 <anastasiya-anoso@mail.ru>
2014-10-07 22:48:38 +0400

    Create Program.java

---
+++ Program.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Nastya on 07.10.2014.
+ */
+
+import view.BoardView;
+
+import java.awt.*;
+
+public class Program extends Canvas {
+
+        public static void main(String[] argS) {
+
+            BoardView board=new BoardView();
+            board.BoardPanel();
+
+        }
+}
+
