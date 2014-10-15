add9e3f92aa11b107a61e61a4be8268ac17e6b2f
Destiny2014 <anastasiya-anoso@mail.ru>
2014-10-15 22:30:24 +0400

    Update Program.java

--- Program.java
+++ Program.java
@@ -2,17 +2,9 @@
  * Created by Nastya on 07.10.2014.
  */
 
-import view.BoardView;
-
-import java.awt.*;
-
-public class Program extends Canvas {
-
-        public static void main(String[] argS) {
-
-            BoardView board=new BoardView();
-            board.BoardPanel();
-
-        }
+public class program {
+    public static void main(String[] args) {
+        Boardview game = new Boardview();
+        game.Go();
+    }
 }
-
